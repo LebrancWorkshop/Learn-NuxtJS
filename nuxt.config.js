c005471ad792bf7de062b4077f95b7061e08bc87
lebrancconvas <lebrancconvas@gmail.com>
2023-06-06 23:34:58 +0700

    ✨ feat: change website title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - basic',
-    title: 'basic',
+    titleTemplate: '%s',
+    title: 'NuxtJS Workshop',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
